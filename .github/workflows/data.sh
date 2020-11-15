3fdb37d7dcfa4559a855ecec77af3064ab4853ca
JJ Merelo <jjmerelo@gmail.com>
2020-11-15 18:57:45 +0100

    Mala idea la config global

--- .github/workflows/data.sh
+++ .github/workflows/data.sh
@@ -4,8 +4,8 @@ cat .github/workflows/header.js | tr '\n' ' ' > "functions/data.js"
 wget https://www.juntadeandalucia.es/institutodeestadisticaycartografia/intranet/admin/rest/v1.0/consulta/38842 -O /tmp/temp.json --no-check-certificate
 cat /tmp/temp.json >> functions/data.js
 echo ";" >> functions/data.js
-git config --global user.email "jjmerelo@gmail.com"
-git config --global user.name "IV-COVID-AND-Bot"
+git config --local user.email "jjmerelo@gmail.com"
+git config --local user.name "IV-COVID-AND-Bot"
 cat functions/data.js
 git add functions/data.js
 git commit -m "Generado fichero con datos"
