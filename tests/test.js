5901f6acd39baf9346dbaa5b0b154796ca473de2
JJ Merelo <jjmerelo@gmail.com>
2021-01-05 13:47:27 +0100

    Nuevo fichero de datos para pruebas
    Obtenido con
    
    ```
    cat samples/sample.json | jq ".data[] | { (.[1].des): .[2].format } " > data-granada.json
    ```

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,9 @@
 const data = require( "../functions/data" );
 
 var weeks = {};
-for ( element in  data.data.data ) {
-  const data_piece =  data.data.data[element];
+for ( element in  data ) {
+  console.log( element, " ", data[element] );
+  const data_piece =  data[element];
   const week = data_piece[0].des;
   const cod = data_piece[1].cod[0];
   if ( !( week in weeks ) ) {
